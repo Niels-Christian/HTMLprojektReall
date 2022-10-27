a7c58dec1b66977f8f4ced4fe6b8b4db163dab32
Jacob <s204418@dtu.dk>
2022-10-27 11:02:36 +0200

    TomcatAhoy

---
+++ src/main/java/ExtendThing.java
@@ -0,0 +1,32 @@
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+
+@WebServlet(name = "HelloServlet", urlPatterns = "/")
+public  class ExtendThing extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Ahoy!");
+        writer.close();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        String uid = req.getParameter("ID");
+        String pwd = req.getParameter("PW");
+
+        if (uid.toLowerCase().equals("qq") && pwd.equals("qq")) {
+            resp.addCookie(new Cookie("session", uid));
+            PrintWriter writer = resp.getWriter();
+            writer.print("Congratulations, " + uid + ", you got in!");
+        } else {
+            resp.sendRedirect("index.html");
+        }
+    }
+}
