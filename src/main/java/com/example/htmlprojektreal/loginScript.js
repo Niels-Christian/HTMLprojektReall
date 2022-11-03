d2d50b42062fafaefe5f9876e523bc152f523abe
Parker <s204814@student.dtu.dk>
2022-11-03 11:22:48 +0100

    Andersen og Parker på eventyr

--- src/main/java/com/example/htmlprojektreal/loginScript.js
+++ src/main/java/com/example/htmlprojektreal/loginScript.js
@@ -1,6 +1,7 @@
 function logintrial() {
     const userName = document.getElementById("username").value;
     const passWord = document.getElementById("password").value;
+
     if (userName == "qq" && passWord == "qq") {
 
         return true;
