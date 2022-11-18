36bf955382c762d99e389e2e2024706ff2538176
Parker <s204814@student.dtu.dk>
2022-11-18 12:04:54 +0100

    Hopefully test branch

---
+++ src/main/java/LoginService.java
@@ -0,0 +1,10 @@
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("index.html")
+public class LoginService {
+    @GET
+    public String testAhoy(){
+        return ('Ahoy');
+    }
+}
