da2dbb7711f26b701537e111999ca5c462450328
Marcus Andersen <marcusbuur1@hotmail.coom>
2023-01-08 20:18:00 +0100

    DatabaseResult renamed to DatabaseInfo cuz I can

--- src/main/java/DatabaseInfo.java
+++ src/main/java/DatabaseInfo.java
@@ -2,10 +2,9 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Date;
 
 
-public class DatabaseResults {
+public class DatabaseInfo {
 
     public static void main(String[] args) {
         String query = "select Name, CPR, ECG from PatientInfo";
