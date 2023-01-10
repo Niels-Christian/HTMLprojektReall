5254231a67c0a4c334c567e7f5749b4fa8676a1e
Marcus Andersen <marcusbuur1@hotmail.coom>
2023-01-10 12:19:13 +0100

    Parker nødhjælp

--- src/main/java/ExtendThing.java
+++ src/main/java/ExtendThing.java
@@ -28,7 +28,7 @@ public  class ExtendThing extends HttpServlet {
             PrintWriter writer = resp.getWriter();
             writer.print("Congratulations, " + uid + ", you got in!");
         } else {
-            resp.sendRedirect("index.html");
+            resp.sendRedirect("PersonaleHome.html");
         }
     }
 }
