b3117913c32c95020cbbce85fdbae7bb51404645
Jacob <s204418@dtu.dk>
2023-01-10 13:45:27 +0100

    JSON del

---
+++ src/main/java/PatientService.java
@@ -0,0 +1,32 @@
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import service.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("patients")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PatientService{
+
+    @GET
+    public List<Patient> getPatient(){
+        ArrayList<Patient> patients = new ArrayList<>();
+        Patient patient = new Patient();
+        patient.setCPR("1234567890");
+        patient.setFornavn("Holger Danske");
+        patients.add(patient);
+
+        return patients;
+    }
+
+    @POST
+    public Patient postPatient(Patient patient){
+        String navn = patient.getFornavn();
+        navn = "Hr." + navn;
+        patient.setFornavn(navn);
+        return patient;
+
+    }
+}
