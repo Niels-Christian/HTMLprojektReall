b032e0aebb6bfa73c65a693006df31d8a750fd4b
Jacob <s204418@dtu.dk>
2023-01-11 10:59:39 +0100

    JSON del 2.0

--- src/main/java/PatientService.java
+++ src/main/java/PatientService.java
@@ -1,32 +1,31 @@
-//package service;
-//
-//import jakarta.ws.rs.Consumes;
-//import jakarta.ws.rs.GET;
-//import jakarta.ws.rs.Path;
-//import jakarta.ws.rs.Produces;
-//import jakarta.ws.rs.core.MediaType;
-//
-//import java.util.ArrayList;
-//import java.util.Arrays;
-//import java.util.Collection;
-//import java.util.List;
-//
-//@Path("patients")
-//@Produces(MediaType.APPLICATION_JSON)
-//@Consumes(MediaType.APPLICATION_JSON)
-//public class PatientService {
-//
-//    @GET
-//    public List<Patient> getPatient(){
-//        //TODO Dummy data - replace!
-//        ArrayList<Patient> patients = new ArrayList<>();
-//        Patient patient = new Patient();
-//        patient.cpr="1110109996";
-//        patient.name="Juliane Test Jørgensen";
-//        patients.add(patient);
-//        //---
-//        return patients;
-//    }
-//
-//}
-//
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import service.Patient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Path("patients")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class PatientService{
+
+    @GET
+    public List<Patient> getPatient(){
+        ArrayList<Patient> patients = new ArrayList<>();
+        Patient patient = new Patient();
+        patient.setCPR("1234567890");
+        patient.setFornavn("Holger Danske");
+        patients.add(patient);
+
+        return patients;
+    }
+
+    @POST
+    public Patient postPatient(Patient patient){
+        String navn = patient.getFornavn();
+        navn = "Hr." + navn;
+        patient.setFornavn(navn);
+        return patient;
+    }
+}
