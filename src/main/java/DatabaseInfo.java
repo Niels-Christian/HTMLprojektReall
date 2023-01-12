2106337b5d1d091ec27e0738be4b91a9ded53984
Marcus Andersen <marcusbuur1@hotmail.coom>
2023-01-12 09:46:36 +0100

    cpr matching java-sql

--- src/main/java/DatabaseInfo.java
+++ src/main/java/DatabaseInfo.java
@@ -2,27 +2,34 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.util.Scanner;
 
 
 public class DatabaseInfo {
 
     public static void main(String[] args) {
-        String query = "select Name, CPR, ECG from PatientInfo";
         Connection conn = null;
         Statement stmt = null;
+        Scanner scan = new Scanner(System.in);
+        String is = scan.next();
+
         try { //
             Class.forName("com.mysql.cj.jdbc.Driver");
             conn = DriverManager.getConnection("jdbc:mysql://mysql-db.caprover.diplomportal.dk/s215846?" +
                     "user=s215846&password=E2yMFR0C4Cs7u3Bc5gsih");
             stmt = conn.createStatement();
+            String query = "select name, cpr, ecg, note from PatientInfo where cpr='" + is +"'";
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 String name = rs.getString(1);
                 String cpr = rs.getString(2);
-                Double ecg = rs.getDouble(3);
-                System.out.println("Name:" + name);
-                System.out.println("CPR:" + cpr);
-                System.out.println("ECG:" + ecg);
+                String ecg = rs.getString(3);
+                String note = rs.getString(4);
+
+                System.out.println("Name: " + name);
+                System.out.println("CPR: " + cpr);
+                System.out.println("ECG: " + ecg);
+                System.out.println("Note: " + note);
                 System.out.println("");
             }
             rs.close();
